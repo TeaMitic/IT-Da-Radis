d47dfeaaa0f7f4f99214a8820fb8465ad7ed62b2
miljapejcic <91380129+miljapejcic@users.noreply.github.com>
2022-02-01 18:06:47 +0100

    gotove fje za jobAdController

--- Backend/routes/jobAdRoutes.js
+++ Backend/routes/jobAdRoutes.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const router = express.Router();
 
-const { CreateJobAd, DeleteJobAd } = require('../controllers/jobAdController')
+const { CreateJobAd,
+        DeleteJobAd,
+        GetAllJobAds,
+        GetCompaniesJobAds,
+        GetJobAdByID,
+        UpdateJobAd,
+        AddTagToJobAd,
+        DeleteTagToJobAd} = require('../controllers/jobAdController')
 
 router.post('/createJobAd/:companyID', CreateJobAd)
 router.delete('/deleteJobAd/:jobAdID', DeleteJobAd)
+router.get('/getAllJobAds', GetAllJobAds)
+router.get('/getCompaniesJobAds/:companyID', GetCompaniesJobAds)
+router.get('/getJobAdByID/:jobAdID', GetJobAdByID)
+router.put('/updateJobAd/:jobAdID', UpdateJobAd)
+router.put('/addTag/:jobAdID', AddTagToJobAd)
+router.put('/deleteTag/:jobAdID', DeleteTagToJobAd)
 
 module.exports = router
