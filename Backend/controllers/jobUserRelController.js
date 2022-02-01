263b9941c57f58b266467ab24759b9a233cbfec4
TeaMitic <tea.mitic20@gmail.com>
2022-02-01 20:42:51 +0100

    relacija izmedju user-a i jobAd
    odradjeni neki apiji

--- Backend/controllers/jobUserRelController.js
+++ Backend/controllers/jobUserRelController.js
@@ -1,2 +1,37 @@
 const mongoose = require('mongoose')
 const {JobUserRel} = require('../models/jobUserRelModel');
+
+const CreateJobApplication= async (req,res)=>{
+    try{
+        const jobApplication= await JobUserRel.create({
+            jobID: req.body.jobID,
+            userID: req.body.userID,
+            cv: req.body.cv,
+            userEmail: req.body.userEmail,
+            userTel: req.body.userTel
+        })
+        res.status(200).send('Uspesno napravljeno')
+        // console.log(jobApplication)
+    }
+    catch(err){
+        res.status(500).send(err.message)
+    }
+}
+const DeleteJobApplication = async (req, res)=>{
+    try{
+        const id = req.params.id
+        await JobUserRel.deleteOne({ _id: id })
+        .then(()=>{
+            res.status(200).send("Uspesno izbrisano")
+        })                         
+    }
+    catch(err){
+        res.status(500).send(err.message)
+    }
+
+}
+
+module.exports={
+    CreateJobApplication,
+    DeleteJobApplication
+}
